cbbebae4e8bd654feaf04815703c28adf63ed1af
gyus-e <giuseppe.amato8@studenti.unina.it>
2025-07-24 11:04:02 +0200

    using chroma ephemeral client for colab

--- rag_service/utils/chroma.py
+++ rag_service/utils/chroma.py
@@ -1,3 +1,4 @@
 import chromadb
 
-chroma_client = chromadb.HttpClient()
+# chroma_client = chromadb.HttpClient() # Use with ollama server running on docker
+chroma_client = chromadb.EphemeralClient()  # Use when running on colab

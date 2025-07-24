f206af5109e85cf24108f1a43faafe62b48a242e
gyus-e <giuseppe.amato8@studenti.unina.it>
2025-07-24 14:57:15 +0200

    better questions

--- cag_service/test_utils/questions_mock.py
+++ cag_service/test_utils/questions_mock.py
@@ -1,7 +1,20 @@
-questions = [
+questions_simple = [
     "Colore dell'inchiostro nel \"Delle famiglie nobili napoletane di Scipione Ammirato parte prima\"?",
     "Che tipo di illustrazione accompagna l'opera \"Delle famiglie nobili napoletane di Scipione Ammirato parte prima\"?",
     "Sono presenti anomalie nella numerazione delle pagine di alcuni libri? Se sí, quali?",
     "Quale tipo di legatura ha il \"Marci Tullii Ciceronis Orationes. Superiorum permissu\"?",
     "Qual è il contributo specifico di Leonardo Liparulo all'opera originale di Andrea d'Isernia?",
+]
+
+questions = [
+    "Quali incunaboli sono stati pubblicati a Venezia e in quali anni?",
+    "Descrivi le caratteristiche fisiche e lo stato di conservazione dell'incunabolo di Caracciolo Roberto (4B1)",
+    "Confronta le legature e le decorazioni degli incunaboli 4B3 e 4C2",
+    "Quali autori classici vengono citati più frequentemente nelle opere della collezione e in quale contesto?",
+    "Quali stampatori veneziani hanno collaborato con più autori presenti nella collezione e quali caratteristiche hanno in comune le loro pubblicazioni?",
+    "Quali filosofi vengono citati nel Liber tertius dell'opera di Achillini Alessandro e quante volte viene menzionato Averroè nell'intero testo?",
+    "Come si riflette l'influenza di Platone nelle opere della collezione Pontaniana?",
+    "Quali marche tipografiche sono presenti negli incunaboli della collezione e cosa rappresentano?",
+    "Chi è Tommaso di Alessandria e qual è il suo ruolo nelle opere della collezione?",
+    "Riassumi i principali temi religiosi trattati nella collezione, citando specifici esempi dai testi",
 ]
